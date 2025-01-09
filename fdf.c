2b6e3f98ae219cd602990912846dccb375d7714a
Gabriel Cesar Nunes Pezoa <gcesar-n@c3r3p4.42sp.org.br>
2025-01-09 19:12:08 -0300

    kartoffel

--- fdf.c
+++ fdf.c
@@ -6,27 +6,13 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:24:58 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/01/09 17:30:22 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/01/09 19:11:55 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	free_memory(int **matrix, int row)
-{
-	int	i;
-
-	i = 0;
-	while (i < row)
-	{
-		free(matrix[i]);
-		i++;
-	}
-	free (matrix);
-	return ;
-}
-
-int	col_counter(char *line)
+static int	col_counter(char *line)
 {
 	int	x;
 
@@ -49,7 +35,7 @@ int	col_counter(char *line)
 	return (x);
 }
 
-int	**create_matrix(int line_amount, int col)
+/*int	**create_matrix(int line_amount, int col)
 {
 	int	**matrix;
 	int	i;
@@ -69,33 +55,30 @@ int	**create_matrix(int line_amount, int col)
 		i++;
 	}
 	return (matrix);
-}
+}*/
 
-char	**populate_matrix(int line_amount, char *matrix)
+static char	**populate_matrix(int line_amount, char **matrix)
 {
+	char	**result;
 	int		i;
 	int		j;
-	char	*result[256];
 
+	result = malloc(sizeof(int *));
 	i = 0;
 	j = 0;
 	while (i < line_amount)
 	{
 		if (matrix[i][j] == '\n')
-		{
 			j++;
-		}
 		else if (matrix[i][j] == '\0')
-		{
-			return (matrix);
-		}
+			return (result);
 		result[i][j] = matrix[i][j];
 		i++;
 	}
-	return (&matrix);
+	return (result);
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
 	int		fd;
 	int		row;
@@ -104,10 +87,10 @@ int	main(int argc, char *argv[])
 
 	row = 0;
 	if (argc != 2)
-		return (ft_printf("Incorrect arg count"), 1);
+		return (ft_printf("Incorrect arg count"));
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
-		return (ft_printf("Error opening the file."), 1);
+		return (ft_printf("Error opening the file."));
 	mango_loko = get_next_line(fd);
 	while (mango_loko != NULL)
 	{
@@ -115,7 +98,7 @@ int	main(int argc, char *argv[])
 		row++;
 		free(mango_loko);
 		mango_loko = get_next_line(fd);
-		ft_printf("%d \n", populate_matrix(4, mango_loko));
+		ft_printf("%d \n", populate_matrix(4, (char **)mango_loko));  //teste
 	}
 	ft_printf("%d %d \n", col, row);
 	close (fd);
